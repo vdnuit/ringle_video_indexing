6e45db317adc3ed504e7571ee7454c05c918944f
vdnuit <97578112+vdnuit@users.noreply.github.com>
2023-02-18 11:53:51 +0900

    subtitle+search

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
-import ReactPlayer from "react-player/lazy";
-import { useState, useRef, useCallback } from "react";
+import ReactPlayer from "react-player";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { wait } from "@testing-library/user-event/dist/utils";
 
-const VideoPlayer = ({}) => {
+export default function App() {
+  //동영상 재생에 필요
+  const [playing, setPlaying] = useState(false);
   const [playIndex, setPlayIndex] = useState(0);
-  const [, updateState] = useState();
-  const forceUpdate = useCallback(() => updateState({}), []);
-  const [time, setTime] = useState(0);
-  const playerRef = useRef();
-  const onTime = () => {
-    setTime(30);
+  const player = useRef();
+
+  //검색창 처리
+  const [search, setSearch] = useState("");
+  const onSearch = (e) => {
+    setSearch(e.target.value);
   };
+
+  //자막 링크
+  const SUBTITLE =
+    "https://ringlecontents.s3.ap-northeast-2.amazonaws.com/webinar/caption/628/1.vtt";
+  const [trackLang, setTrackLang] = useState();
+  //자막 넣기 위한 코드
+  useEffect(() => {
+    const textTracks = player.current.getInternalPlayer()?.textTracks;
+
+    for (var i = 0; textTracks?.length && i < textTracks.length; i++) {
+      // For the 'subtitles-off' button, the first condition will never match so all will subtitles be turned off
+      if (textTracks[i].language === trackLang) {
+        textTracks[i].mode = "showing";
+      } else {
+        textTracks[i].mode = "hidden";
+      }
+    }
+  }, [trackLang]);
+
+  //재생 목록(클립 여러 개일 때 대비)
+  const playList = [
+    {
+      index: 1,
+      url: "https://ringlecontents.s3.ap-northeast-2.amazonaws.com/webinar/video/628/1.mp4",
+    },
+  ];
+
+  //타임스탬프에 따라 이동
+  const [time, setTime] = useState(0);
   const on40 = () => {
     setTime(40 + 1);
     // 야매로 중복클릭 가능하게 수정함
@@ -20,43 +51,49 @@ const VideoPlayer = ({}) => {
       setTime(40);
     }, 1);
   };
-  const playList = [
-    {
-      index: 1,
-      url: "https://ringlecontents.s3.ap-northeast-2.amazonaws.com/webinar/video/604/1.mp4",
-    },
-  ];
-
-  if (playList === null) return <p>Loading...</p>;
 
   return (
-    <>
-      <h2>Player Test</h2>
+    <div className="App">
+      <h1>링글 동영상 데모</h1>
+      <input
+        onChange={onSearch}
+        value={search}
+        placeholder="검색할 단어를 입력하세요"
+      />
       <ReactPlayer
-        // @ts-ignore
-        ref={playerRef}
-        // @ts-ignore
+        ref={player}
         url={playList[playIndex].url + `#t=${time}`}
-        playing
         controls
         muted
         progressInterval={1000}
         pip={true}
-        width={"800px"}
-        height={"500px"}
+        config={{
+          file: {
+            attributes: {
+              crossOrigin: "anonymous",
+            },
+            tracks: [
+              {
+                kind: "subtitles",
+                src: SUBTITLE,
+                srcLang: "en",
+                default: false,
+                mode: trackLang === "en" ? "showing" : "hidden",
+              },
+            ],
+          },
+        }}
+        playing={playing}
+        loop
       />
-      <button onClick={onTime}>20</button>
-      <button onClick={on40}>30</button>
-    </>
-  );
-};
+      {trackLang}
+      <button onClick={() => setTrackLang("en")}>자막 키기</button>
+      <button onClick={() => setTrackLang()}>자막 끄기</button>
 
-function App() {
-  return (
-    <div className="App">
-      <VideoPlayer />
+      <button onClick={() => setPlaying(!playing)}>
+        {playing ? "정지" : "재생"}
+      </button>
+      <button onClick={on40}>30</button>
     </div>
   );
 }
-
-export default App;
